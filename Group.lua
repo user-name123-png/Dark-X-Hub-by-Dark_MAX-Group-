d6c6a7fdf03d2efb15d7c58255a8c68c61b65994
user-name123-png <mgkiller5114.ez@gmail.com>
2025-03-20 03:54:30 +0700

    Update and rename .gitignore to Group.lua

---
+++ Group.lua
@@ -0,0 +1,22 @@
+local workspace = game:GetService("Workspace")
+
+-- ฟังก์ชันรันสคริปต์ที่ต้องการ
+local function DeadRails()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX/refs/heads/main/Dead%20Rails.lua"))()
+end
+
+local function NinjaLegends()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX/refs/heads/main/Ninja%20Legands.lua"))()
+end
+
+-- ฟังก์ชันตรวจสอบโฟลเดอร์และรันสคริปต์ที่เหมาะสม
+local function checkAndRunScripts()
+    if workspace:FindFirstChild("RuntimeItems") then
+        DeadRails()
+    elseif workspace:FindFirstChild("Hoops") then
+        NinjaLegends()
+    end
+end
+
+-- ตรวจสอบเมื่อเริ่มเกม
+checkAndRunScripts()

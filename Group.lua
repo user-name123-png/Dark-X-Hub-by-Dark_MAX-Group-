27aa41a074e5659fa0438a7d5b52b68b0b6f991d
user-name123-png <mgkiller5114.ez@gmail.com>
2025-04-13 22:28:00 +0700

    Update Group.lua

--- Group.lua
+++ Group.lua
@@ -1,24 +1,28 @@
 local workspace = game:GetService("Workspace")
 
 -- ฟังก์ชันรันสคริปต์ที่ต้องการ
+local function infiniteyield()
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+end
+
 local function DeadRails()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX/refs/heads/main/Dead%20Rails.lua"))()
+    infiniteyield()
 end
 
 local function NinjaLegends()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX/refs/heads/main/Ninja%20Legands.lua"))()
+    infiniteyield()
 end
 
 local function TheStrongestBattlegrounds()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX/refs/heads/main/The%20Strongest%20Battlegrounds.lua"))()
+    infiniteyield()
 end
 
 local function Forsaken()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX/refs/heads/main/Forsaken.lua"))()
-end
-
-local function infiniteyield()
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'),true))()
+    infiniteyield()
 end
 
 -- ฟังก์ชันตรวจสอบโฟลเดอร์และรันสคริปต์ที่เหมาะสม

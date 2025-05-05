7090660732f285d51496fbe2ed0babfb064653fa
user-name123-png <mgkiller5114.ez@gmail.com>
2025-05-05 09:56:15 +0700

    Update Group รวม Configs.lua

--- Group รวม Configs.lua
+++ Group รวม Configs.lua
@@ -1,4 +1,4 @@
 getgenv().Configuration = {
-    Themes = "GrapeTheme" -- "DarkTheme" // "LightTheme" // "GrapeTheme" // "BloodTheme" // "Ocean" // "Midnight" // "Sentinel" // "Synapse"
+    Themes = "DarkTheme" -- "DarkTheme" // "LightTheme" // "GrapeTheme" // "BloodTheme" // "Ocean" // "Midnight" // "Sentinel" // "Synapse"
 }
 loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX-Group-/refs/heads/main/Group.lua"))()

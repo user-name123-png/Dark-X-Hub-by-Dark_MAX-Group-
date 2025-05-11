e36225c2743e9ff9ff7866020fd87832ef507de4
user-name123-png <mgkiller5114.ez@gmail.com>
2025-05-11 13:33:15 +0700

    Update Group.lua

--- Group.lua
+++ Group.lua
@@ -1,4 +1,4 @@
 getgenv().Configuration = {
-    Themes = "Ocean" -- "DarkTheme" // "LightTheme" // "GrapeTheme" // "BloodTheme" // "Ocean" // "Midnight" // "Sentinel" // "Synapse"
+    Themes = "DarkTheme" -- "DarkTheme" // "LightTheme" // "GrapeTheme" // "BloodTheme" // "Ocean" // "Midnight" // "Sentinel" // "Synapse"
 }
-loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX-Group-/refs/heads/main/--GUI%20Group.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX-Group-/refs/heads/main/obf_Group.lua"))()

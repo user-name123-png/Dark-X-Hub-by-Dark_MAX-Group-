8fbf9b67cd6d5936a0eac21f84ba80a261e69b64
user-name123-png <mgkiller5114.ez@gmail.com>
2025-05-11 13:00:49 +0700

    Add files via upload

---
+++ Group.lua
@@ -0,0 +1,4 @@
+getgenv().Configuration = {
+    Themes = "DarkTheme" -- "DarkTheme" // "LightTheme" // "GrapeTheme" // "BloodTheme" // "Ocean" // "Midnight" // "Sentinel" // "Synapse"
+}
+loadstring(game:HttpGet("https://raw.githubusercontent.com/user-name123-png/Dark-X-Hub-by-Dark_MAX-Group-/refs/heads/main/Group.lua"))()
